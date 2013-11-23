4e160bf917b6b495be42fa6a2c00829a02f7262a
Jeff Casimir <jeff@casimircreative.com>
2013-11-22 22:51:29 -0700

    First sketch of Luhn algorithm implementation

---
+++ README.markdown
@@ -0,0 +1,5 @@
+## Data Structures & Algorithms
+
+In the process of becoming or practicing as a web application developer, it's easy to lose sight of Computer Science.
+
+This collection of exercises and experiments seeks to help you build an understanding of those foundations.
---
+++ luhn_algorithm/luhn_test.rb
@@ -0,0 +1,24 @@
+gem 'minitest'
+require 'minitest/autorun'
+require_relative 'luhn_validator'
+
+class LuhnTest < Minitest::Test
+  # This test suite is not meant to be exhaustive,
+  # just a start. Write your own tests!
+  #
+  # It's very likely that you'll want to 
+
+  attr_reader :validator
+
+  def setup
+    @validator = LuhnValidator.new
+  end
+
+  def test_it_validates_the_example_account_number
+    assert validator.validate("79927398713")
+  end
+
+  def test_it_finds_the_check_digit_for_an_identifier
+    assert validator.check_digit_for("7992739871")
+  end
+end
