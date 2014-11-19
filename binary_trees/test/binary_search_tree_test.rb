a4d19749a389f7e7b5893e6854b9d225465ec023
worace <horace.d.williams@gmail.com>
2014-11-19 11:59:42 -0700

    add a few more requirements and skip all but initial test

--- binary_trees/test/binary_search_tree_test.rb
+++ binary_trees/test/binary_search_tree_test.rb
@@ -13,10 +13,12 @@ describe Node do
   end
 
   it "has a left node" do
+    skip
     assert_equal 1, Node.new(0, Node.new(1)).left.data
   end
 
   it "has a right node" do
+    skip
     assert_equal 2, Node.new(0, nil, Node.new(2)).right.data
   end
 end
@@ -27,10 +29,12 @@ describe BinarySearchTree do
   end
   describe "#root_node" do
     it "is nil for an empty list" do
+      skip
       assert_nil BinarySearchTree.new.root_node
     end
 
     it "adds a new root node when pushed" do
+      skip
       @tree.push(1)
       assert_equal 1, @tree.root_node.data
     end
@@ -41,24 +45,28 @@ describe BinarySearchTree do
     # provide a nicer interface, we will accept raw numbers as inputs to
     # push, and handle wrapping the data in a Node internally
     it "adds data smaller than the root node to the left of the root" do
+      skip
       @tree.push(5) #root
       @tree.push(4)
       assert_equal 4, @tree.root_node.left.data
     end
 
     it "adds data equal to the root node to the left of the root" do
+      skip
       @tree.push(5) #root
       @tree.push(5)
       assert_equal 5, @tree.root_node.left.data
     end
 
     it "adds data larger than the root node to the right of the root" do
+      skip
       @tree.push(5) #root
       @tree.push(7)
       assert_equal 7, @tree.root_node.right.data
     end
 
     it "continues adding smaller data to the left down the tree" do
+      skip
       @tree.push(5) #root
       @tree.push(4)
       @tree.push(3)
@@ -66,6 +74,7 @@ describe BinarySearchTree do
     end
 
     it "continues adding larger data to the right down the tree" do
+      skip
       @tree.push(5) #root
       @tree.push(6)
       @tree.push(7)
@@ -76,15 +85,18 @@ describe BinarySearchTree do
   describe "#count" do
     # count tells us the number of nodes in the tree
     it "is 0 for an empty tree" do
+      skip
       assert_equal 0, @tree.count
     end
 
     it "is 1 for a tree with a root node" do
+      skip
       @tree.push(5)
       assert_equal 1, @tree.count
     end
 
     it "continues to count elements as they are added" do
+      skip
       @tree.push(6)
       @tree.push(4)
       @tree.push(3)
@@ -96,15 +108,18 @@ describe BinarySearchTree do
   describe "#include?" do
     # include? tells us whether a piece of data exists in the tree
     it "is false for an empty tree" do
+      skip
       assert_equal false, @tree.include?(5)
     end
 
     it "is true for the data of the root node" do
+      skip
       @tree.push(4)
       assert_equal true, @tree.include?(4)
     end
 
     it "finds data lower in the tree" do
+      skip
       @tree.push(4)
       @tree.push(6)
       @tree.push(3356)
@@ -121,10 +136,12 @@ describe BinarySearchTree do
     # the root to the furthest leaf
 
     it "is 0 for an empty tree" do
+      skip
       assert_equal 0, @tree.max_depth
     end
 
     it "gets big if we add lots of the same data" do
+      skip
       @tree.push(5)
       @tree.push(5)
       @tree.push(5)
@@ -135,6 +152,7 @@ describe BinarySearchTree do
     end
 
     it "grows more slowly if we add data which creates a balanced tree" do
+      skip
       @tree.push(5)
       @tree.push(4)
       @tree.push(6)
@@ -146,10 +164,12 @@ describe BinarySearchTree do
 
   describe "#to_array" do
     it "is empty for empty tree" do
+      skip
       assert_equal [], @tree.to_array
     end
 
     it "pulls data from the tree into an array, starting with the left branches of the tree" do
+      skip
       @tree.push(5)
       @tree.push(4)
       @tree.push(6)
@@ -159,14 +179,35 @@ describe BinarySearchTree do
     end
   end
 
+  describe "#sort" do
+    it "returns empty array for empty tree" do
+      skip
+      assert_equal [], @tree.sort
+    end
+
+    it "returns a sorted array of the tree elements" do
+      skip
+      @tree.push(5)
+      @tree.push(4)
+      @tree.push(6)
+      @tree.push(7)
+      @tree.push(3)
+      @tree.push(2)
+      @tree.push(2102)
+      assert_equal [2,3,4,5,6,7,2102], @tree.sort
+    end
+  end
+
   describe "#min" do
     # note that given the inherent left/right structure of a binary search tree
     # it is possible to find the min or max elements without searching the entire tree
     it "is nil for an empty tree" do
+      skip
       assert_nil @tree.min
     end
 
     it "finds the minimum value in the tree" do
+      skip
       @tree.push(5)
       @tree.push(4)
       @tree.push(6)
@@ -181,10 +222,12 @@ describe BinarySearchTree do
     # note that given the inherent left/right structure of a binary search tree
     # it is possible to find the min or max elements without searching the entire tree
     it "is nil for an empty tree" do
+      skip
       assert_nil @tree.max
     end
 
     it "finds the minimum value in the tree" do
+      skip
       @tree.push(5)
       @tree.push(4)
       @tree.push(6)
@@ -192,6 +235,35 @@ describe BinarySearchTree do
       @tree.push(3)
       @tree.push(1)
       assert_equal 7, @tree.max
+    end
+  end
+
+  describe "#post_ordered" do
+    # post_order should return the elements of the tree from a 'bottom-up' perspective,
+    # starting at the bottom-left-most element, then working right, and then progressively
+    # up the tree
+    # so, for example the tree:
+    #        4
+    #       / \
+    #      2   5
+    #     / \
+    #    1   3
+    # should produce post_ordered output [1,3,2,5,4]
+
+    it "should be empty for empty tree" do
+      skip
+      assert_equal [], @tree.post_ordered
+    end
+
+    it "should give back elements in post-order" do
+      skip
+      @tree.push(4)
+      @tree.push(2)
+      @tree.push(5)
+      @tree.push(1)
+      @tree.push(3)
+
+      assert_equal [1,3,2,5,4], @tree.post_ordered
     end
   end
 end
